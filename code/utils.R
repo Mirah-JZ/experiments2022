ef9e4ae4d8f5af8290c771c9317ae156f65e946b
Mirah-JZ <283558097@qq.com>
2022-07-23 01:26:54 +0100

    Updates

--- code/utils.R
+++ code/utils.R
@@ -2,17 +2,20 @@
 # Utils
 
 # -----------------------------------------------------------------------------
-# conformal score. for conformalCV main func
+# conformal score.
 # -----------------------------------------------------------------------------
 # residual based conformal score
-conformalScore <- function(Y, Yhat, quantiles){
-  if (quantiles==0) {
-    score <- pmax(Yhat - Y, Y - Yhat) # for mean pred
+conformalScore <- function(Y, Yhat, quantiles=FALSE){
+  if (quantiles==FALSE) {
+    score <- pmax(Yhat - Y, Y - Yhat) # for mean pred Y is vector
   } else {
-    score <- pmax(Yhat[, 1] - Y, Y - Yhat[, 2]) # for CQR
+    score <- pmax(Yhat[, 1] - Y, Y - Yhat[, 2]) # for CQR Y interval
   }
   return(score)
 }
+
+
+
 
 # -----------------------------------------------------------------------------
 # Generate a list of indices for cross-validation. 
@@ -33,7 +36,7 @@ gen_cv_ids <- function(n, nfolds, offset = 0){
 
 
 # -----------------------------------------------------------------------------
-# weight conformal cutoff, for conformalCV main func
+# weight conformal cutoff
 # -----------------------------------------------------------------------------
 weightedConformalCutoff <- function(score, weight, qt){
   
@@ -49,15 +52,29 @@ weightedConformalCutoff <- function(score, weight, qt){
   score <- score[ord]
   cw <- cumsum(weight)
   
-  inds <- find_inds(cw, qt)    
+  inds <- find_inds(cw, qt) # find i such that cumsum(w)>=qt
   cutoff <- score[inds]
   return(cutoff)
 }
 
 
+# Weighted quantile function from Tibshirani R package implimentation
+# score input is the abs Val set residuals
+# prob=1-alpha, 
+# w is the weight of val and test points, input is already normalised by totw
+weightedquantile <- function(score, prob=0.9, w, sorted=FALSE) {
+  if (is.null(w)) w = rep(1,length(score))
+  if (!sorted) { o = order(score); score = score[o]; w = w[o] }
+  i = which(cumsum(w) >= prob) # if w not normalised use w/sum(w) instead of w
+  if (length(i)==0) return(Inf) # Can happen with infinite weights
+  else return(score[min(i)])
+}
+
+
 # -----------------------------------------------------------------------------
-## wrapper for randomForest::randomForest
+## randomForest
 # -----------------------------------------------------------------------------
+# if Y is categorical, res=predict(... , type="prob")
 RF <- function(Y, X, Xtest){
   # if Y continuous
   fit <- randomForest::randomForest(x = X, y = Y)
@@ -67,7 +84,7 @@ RF <- function(Y, X, Xtest){
 
 
 # -----------------------------------------------------------------------------
-# quantile random forest. wrapper for grf::quantile_forest 
+# quantile random forest.
 # -----------------------------------------------------------------------------
 # in main func definitions here, by default quantiles = c(0.05, 0.95)
 quantRF <- function(Y, X, Xtest, quantiles, ...){
@@ -83,7 +100,64 @@ quantRF <- function(Y, X, Xtest, quantiles, ...){
 
 
 # -----------------------------------------------------------------------------
-## wrapper for gbm::gbm 
+## Quantile Boosting
+# -----------------------------------------------------------------------------
+QBoosting <- function(Y, X, Xtest, quantiles, n.trees = 100){
+  if (class(X) != "data.frame"){
+    X <- as.data.frame(X)
+    Xtest <- as.data.frame(Xtest)
+    names(Xtest) <- names(X)
+  }
+  data <- data.frame(Y = Y, X)
+  fit <- gbm::gbm(Y ~ ., distribution = list(name = "quantile", alpha = quantiles[1]), 
+                  data = data, n.trees = n.trees)
+  res <- predict(fit, Xtest, type = "response", n.trees = n.trees)
+  if (length(quantiles) == 2){
+    fit2 <- gbm::gbm(Y ~ ., distribution = list(name = "quantile", alpha = quantiles[2]), 
+                     data = data, n.trees = n.trees)
+    res2 <- predict(fit2, Xtest, type = "response", n.trees = n.trees)
+    res <- cbind(res, res2)
+  }
+  return(res)
+}
+
+
+# -----------------------------------------------------------------------------
+## Quantile Bart
+# -----------------------------------------------------------------------------
+QBART <- function(Y, X, Xtest, quantiles,
+                      ndpost = 100){
+  if (class(X) != "data.frame"){
+    X <- as.data.frame(X)
+    Xtest <- as.data.frame(Xtest)
+    names(Xtest) <- names(X)
+  }
+  fit <- bartMachine::bartMachine(X, Y, verbose = FALSE)
+  res <- bartMachine::calc_prediction_intervals(
+      fit, new_data = Xtest,pi_conf = 0.95)$interval
+  
+  return(res)
+}
+
+
+# -----------------------------------------------------------------------------
+## Bart
+# -----------------------------------------------------------------------------
+BART <- function(Y, X, Xtest,
+                 ndpost = 100, ...){
+  if (class(X) != "data.frame"){
+    X <- as.data.frame(X)
+    Xtest <- as.data.frame(Xtest)
+    names(Xtest) <- names(X)
+  }
+  fit <- bartMachine::bartMachine(X, Y, verbose = FALSE)
+  res <- predict(fit, Xtest)
+  return(res)
+}
+
+
+# -----------------------------------------------------------------------------
+## Boosting
 # -----------------------------------------------------------------------------
 Boosting <- function(Y, X, Xtest, n.trees = 100){
   # X Xtest should be dataframes
@@ -121,18 +195,50 @@ spectral <- function(X, K) {
 # -----------------------------------------------------------------------------
 get_trnei1 <- function(tr,A,Atype){
   if (Atype=="dist"){
-    A0 <- A
+    A0<-A
     diag(A0) <-1
     A0 <- A^(-1)
     diag(A0) <- 0
-    A0 <- A0/rowSums(A0) 
+    A0 <- A0/rowSums(A0) # row std inverse dist, can try other dist decay
     trnei <- rowSums(matrix(tr,nrow=n,ncol=n,byrow=TRUE)*A0) 
-    trnei <- ifelse(trnei>0.5,1,0)
+    trnei <- ifelse(trnei>0.5,1,0) # neighbor treatment {0,1}, 0.5 is arbitrary here
   } else if (Atype=="adj") {
-    A0 <- A
+    A0<-A
     A0 <- A0/rowSums(A0) 
     trnei <- rowSums(matrix(tr,nrow=n,ncol=n,byrow=TRUE)*A0)
     trnei <- ifelse(trnei>0.5,1,0)
   }
   return(trnei)
+}
+
+# -----------------------------------------------------------------------------
+# get_Xnei get neighborhood mean X 
+# -----------------------------------------------------------------------------
+get_Xnei <- function(covar,A,Atype){
+  k <- ncol(covar)
+  covnei <- list()
+  if(Atype=="dist") {
+    A0<-A
+    diag(A0) <-1
+    A0 <- A^(-1)
+    diag(A0) <- 0
+    A0 <- A0/rowSums(A0) # row std inverse dist
+    for (i in 1:k) {
+      covnei[[i]] <- rowSums(matrix(covar[,i],nrow=n,ncol=n,byrow=TRUE)*A0)
+    }
+    covnei <- as.data.frame(do.call(cbind,covnei))
+    
+  } else if (Atype=="adj") {
+    A0<-A
+    A0 <- A0/rowSums(A0)
+    for (i in 1:k) {
+      covnei[[i]] <- rowSums(matrix(covar[,i],nrow=n,ncol=n,byrow=TRUE)*A0)
+    }
+    covnei <- as.data.frame(do.call(cbind,covnei))
+    netA <- igraph::graph_from_adjacency_matrix(A,mode="undirected",weighted=NULL)
+    dgr <- rowSums(A) # the node degrees
+    btwnss <- betweenness(netA, weights = NA) # betweeness centrality
+    covnei <- cbind.data.frame(covnei,dgr,btwnss)
+  }
+  return(covnei)
 }
